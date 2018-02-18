d0f10cf675f07d8b8d51327dc142c227a165c425
FTBCraig <ftbcraig@gmail.com>
2018-02-19 00:08:02 +1000

    Changes to formatting

--- btcmarkets.py
+++ btcmarkets.py
@@ -1,20 +1,18 @@
 # First Python application including Api support
-
 # Going to try and pull AUD price of Bitcoin from BTC Market
 
 import urllib.parse
 import requests
-# VARIABLEs
+
+# VAR
+
 main_api = 'https://api.btcmarkets.net'
-
 address = '/market/BTC/AUD/tick'
-
 url = main_api + address
-
 json_data = requests.get(url).json()
-
 json_instrument = json_data['instrument']
 json_price = json_data['lastPrice']
+
 
 print('BtcMarkets')
 print()
