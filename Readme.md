0924a0b8f618d1693ab92d345f0fa75fa7f38b31
FTBCraig <ftbcraig@gmail.com>
2018-02-19 00:05:46 +1000

    New files

---
+++ Readme.md
@@ -0,0 +1,5 @@
+// BtcMarkets Test
+
+Im trying to create an app for my phone that makes it simple to track crypto amounts and prices.
+
+I know there are many but id like to build my own.
---
+++ btcmarkets.py
@@ -0,0 +1,22 @@
+# First Python application including Api support
+
+# Going to try and pull AUD price of Bitcoin from BTC Market
+
+import urllib.parse
+import requests
+# VARIABLEs
+main_api = 'https://api.btcmarkets.net'
+
+address = '/market/BTC/AUD/tick'
+
+url = main_api + address
+
+json_data = requests.get(url).json()
+
+json_instrument = json_data['instrument']
+json_price = json_data['lastPrice']
+
+print('BtcMarkets')
+print()
+print('Pair: ' + json_instrument + "/AUD")
+print('Price: ' + "$"+str(json_price))
